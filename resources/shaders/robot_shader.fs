fd8b8f5bb005e7af716fc4997f8a395135ef33f2
milica <milicagnjatovic18@gmail.com>
2021-01-30 21:30:38 +0100

    Problem sa kretanjem.

--- resources/shaders/robot_shader.fs
+++ resources/shaders/robot_shader.fs
@@ -13,21 +13,41 @@ struct PointLight {
     float quadratic;
 };
 
+struct DirLight {
+    vec3 direction;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
 struct Material {
-    sampler2D texture_diffuse1;
-    sampler2D texture_specular1;
-    sampler2D texture_ambient1;
+    vec4 texture_diffuse1;
+    vec4 texture_specular1;
+    vec4 texture_ambient1;
 
     float shininess;
 };
+// struct Material {
+//     sampler2D texture_diffuse1;
+//     sampler2D texture_specular1;
+//     sampler2D texture_ambient1;
+//
+//     float shininess;
+// };
 
 
 in vec2 TexCoords;
 in vec3 Normal;
 in vec3 FragPos;
-in vec4 Color;
+// in vec4 Color;
+
+in vec4 ambColor;
+in vec4 diffColor;
+in vec4 specColor;
 
 uniform PointLight pointLight;
+uniform DirLight dirLight;
 uniform Material material;
 
 uniform vec3 viewPosition;
@@ -44,20 +64,44 @@ vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
     float distance = length(light.position - fragPos);
     float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
     // combine results
-    vec3 ambient = light.ambient * vec3(texture(material.texture_ambient1, TexCoords));
-    vec3 diffuse = light.diffuse * diff * vec3(texture(material.texture_diffuse1, TexCoords));
-    vec3 specular = light.specular * spec * vec3(texture(material.texture_specular1, TexCoords).xxx);
-    ambient *= attenuation;
+    vec3 ambient = light.ambient * material.texture_ambient1.xyz; //vec3(texture(material.texture_ambient1, TexCoords));
+    vec3 diffuse = light.diffuse * diff * material.texture_diffuse1.xyz;//vec3(texture(material.texture_diffuse1, TexCoords));
+    vec3 specular = light.specular * spec * material.texture_specular1.xyz;//vec3(texture(material.texture_specular1, TexCoords).xxx);
+    ambient *=  attenuation;
     diffuse *= attenuation;
     specular *= attenuation;
-    return (ambient + diffuse + specular);
+    return diffuse;
+    //return (0.1*ambient + diffuse + 0.1*specular);
 }
+
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir)
+{
+    vec3 lightDir = normalize(-light.direction);
+    // diffuse shading
+    float diff = max(dot(normal, lightDir), 0.0);
+    // specular shading
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    // combine results
+    vec3 ambient = light.ambient * material.texture_ambient1.xyz; //vec3(texture(material.texture_ambient1, TexCoords));
+    vec3 diffuse = light.diffuse * diff * material.texture_diffuse1.xyz;//vec3(texture(material.texture_diffuse1, TexCoords));
+    vec3 specular = light.specular * spec * material.texture_specular1.xyz;//vec3(texture(material.texture_specular1, TexCoords).xxx);
+    return diffuse;
+//     return (0.1*ambient + diffuse + 0.1*specular);
+}
+
 
 void main()
 {
+    Material material;
+    material.texture_diffuse1 = diffColor;
+    material.texture_ambient1 = ambColor;
+    material.texture_specular1 = specColor;
+
     vec3 normal = normalize(Normal);
     vec3 viewDir = normalize(viewPosition - FragPos);
     vec3 result = CalcPointLight(pointLight, normal, FragPos, viewDir);
-
-    FragColor = Color;
+    result = CalcDirLight(dirLight, normal, viewDir);
+    FragColor = material.texture_diffuse1;
+//     FragColor = vec4(result, 1.0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -41,8 +41,8 @@ float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
 //figure position
-glm::vec3 robot_position = glm::vec3(0.0f);
-float robot_speed = 2.0f;
+glm::vec3 robot_position = glm::vec3(0.0f, 0.1f, 0.0f);
+float robot_speed = 1.5f;
 float robot_rotate = 0;
 
 int points = 0;
@@ -434,6 +434,11 @@ int main()
         robotShader.setVec3("viewPosition", camera.Position);
         robotShader.setFloat("material.shininess", 32.0f);
 
+        robotShader.setVec3("dirlight.direction", glm::vec3(0.0, 4.0, 0.0));
+        robotShader.setVec3("dirlight.ambient", glm::vec3(0.1));
+        robotShader.setVec3("dirlight.diffuse", glm::vec3(0.5));
+        robotShader.setVec3("dirlight.specular", glm::vec3(1.0));
+
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
         robotShader.setMat4("projection", projection);
@@ -441,9 +446,9 @@ int main()
 
         // pozicioniranje robota
         glm::mat4 model = glm::mat4(1.0f);
+//        model = glm::rotate(model, glm::radians(robot_rotate), glm::vec3(0, 1, 0));
+        model = glm::scale(model, glm::vec3(0.6f));	// it's a bit too big for our scene, so scale it down
         model = glm::translate(model, robot_position); // translate it down so it's at the center of the scene
-        model = glm::scale(model, glm::vec3(0.4f));	// it's a bit too big for our scene, so scale it down
-        model = glm::rotate(model, glm::radians(robot_rotate), glm::vec3(0, 1, 0));
         robotShader.setMat4("model", model);
 
         robotModel.Draw(robotShader);
@@ -551,6 +556,7 @@ void processInput(GLFWwindow *window)
         camera.ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
+
     if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS){
         robot_position.z -= robot_speed*deltaTime;
         robot_rotate = 180.0f;
@@ -590,7 +596,7 @@ void processInput(GLFWwindow *window)
     if(pow(battery_position.x-robot_position.x, 2) + pow(battery_position.z-robot_position.z, 2) < 0.2){
         points ++;
         if(is_speed_treat)
-            robot_speed += 0.1;
+            robot_speed += 0.01;
         new_treat();
     }
 }
